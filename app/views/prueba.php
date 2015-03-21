50c64036b1286fa7c1ae9d6cada7395e23a4d590
HP <jcochoaz@unal.edu.co>
2015-03-21 13:16:10 -0500

    Hice unos cambios a lo realizado en la clase 2

--- app/views/prueba.php
+++ app/views/prueba.php
@@ -10,6 +10,25 @@
         <a href="../clase2/css-persona">Enlace a pagina hecha en bootstrap</a><br>
         <a href="mailto:jcochoaz@unal.edu.co?subject=Dandole al html">Email al milo</a><br>
         <img id="imggatico" src="../../public/assets/img/logo_animales.gif">
+        <table widht="50%" border="2px" align="center">
+            <caption align="left" valign="bottom">Titulo de la tabla<tr>
+            <tr>
+                <th>
+                    Sexo
+                </th>
+                <th>
+                    Edad
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    Masculino
+                </td>
+                <td>
+                    21
+                </td>
+            </tr>
+        </table>
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         <a name="miancla"></a>Texto con ancla
     </body>
